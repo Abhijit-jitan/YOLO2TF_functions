eae32bd07ca9666ce4df830fe0635dd9fe5102f9
Abhijit <55889831+Abhijit-jitan@users.noreply.github.com>
2022-07-05 22:53:00 +0530

    Add files via upload

---
+++ weight_from_dir.py
@@ -0,0 +1,41 @@
+import os
+
+def modify_class_names(name_string):
+    """ Takes class-names as string and modifies the custom.names file"""
+    try:
+        with open(r"data\classes\custom.names","w+") as f:
+            for name in name_string.split(","):
+                f.write(name) 
+                f.write("\n") 
+    except:
+        print("custom.names File Not Found !!!\n")
+        
+    print("Class-Names Modified Successfully !!!\n")
+####
+#modify_class_names("clean,deep-clean")
+
+
+def convert_weight_from_dir(size=416,model="yolov4",tiny=True,theshold=0.2,framework="tf"):
+    weight_in="./yolo_weight_file/"
+    
+    if len(os.listdir(weight_in))>0:
+        for weight in os.listdir(weight_in):
+            if weight.endswith(".weights")==True:
+                weight_file = weight_in + "/" + weight
+                weight_out="./pb_weight_file/"+weight.split(".weights")[0]
+                print("Converting : {}\n".format(weight))
+
+                ## commands
+                main_command ="python save_model.py"+" --weights "+ weight_file + " --input_size " + str(size)+" --model "+model+" --output "+weight_out+" --score_thres "+str(theshold)+" --framework "+framework
+
+                if tiny == True: 
+                    main_command=main_command+" --tiny"
+            
+                #print(main_command)
+                os.system(main_command)
+    else:
+        print("No Weight File Found !!!")
+
+####
+modify_class_names("glove,front,full,back")
+convert_weight_from_dir(size=416,model="yolov4",tiny=True,theshold=0.2,framework="tf")
